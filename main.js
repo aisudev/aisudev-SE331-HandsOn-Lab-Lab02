b5a163ad8b7a5f4edd6d6f2bc98ed6272e2b9a3e
aisudev <tk.thanatorn@gmail.com>
2021-07-08 20:30:26 +0700

    done

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 const app = Vue.createApp({
     data() {
         return {
+            brand: 'SE331',
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
-
+            cart: 0,
+            selectVariant: 0,
+            onSale: true
         }
     },
+
     methods: {
         addToCart() {
             this.cart += 1
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectVariant = index
+        }
+    },
+
+    computed: {
+        title() {
+            if(this.onSale) return this.brand + ' ' + this.product + ' is On Sale'
+
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectVariant].quantity
         }
     }
 })
